85535cd88e243a158f6921c62f97f2aeccb7d76f
Nihal <37813784+iamnihal@users.noreply.github.com>
2019-08-01 14:10:30 +0530

    Update urlchecker.sh

--- urlchecker.sh
+++ urlchecker.sh
@@ -3,27 +3,27 @@
 logo()
 {
 	cat <<"EOT"
-	          ____ _        _                ____          _      
+		  ____ _        _                ____          _      
 		/ ___|| |_ __ _| |_ _   _ ___   / ___|___   __| | ___ 
 		\___ \| __/ _` | __| | | / __| | |   / _ \ / _` |/ _ \
-	         ___) | || (_| | |_| |_| \__ \ | |__| (_) | (_| |  __/
+		 ___) | || (_| | |_| |_| \__ \ | |__| (_) | (_| |  __/
 		|____/ \__\__,_|\__|\__,_|___/  \____\___/ \__,_|\___|
-                                                         
-                                                      			
+
+
 EOT
 }
 
-	clear
-	logo
-	if [ -z "$1" ]
-	then
-		echo "No argument provided!!"
-		sleep 2
-	else
+clear
+logo
+if [ -z "$1" ]
+then
+	echo -e "\033[1;31mNo argument provided!!"
+	sleep 2
+else
 	n=$(cat $1 | wc -l)
-	echo "+----------------------+"
-	echo -e "|  $n urls to be check  |"
-	echo "+----------------------+"
+	echo -e "\033[1;31m+----------------------+"
+	echo -e "\033[1;31m|  $n urls to be check  |"
+	echo -e "\033[1;31m+----------------------+"
 	i=1
 	for urls in $(cat $1)
 	do
@@ -31,7 +31,7 @@ EOT
 		if [ "$?" -eq "0" ]
 		then
 			code=$(curl -Is "$urls" | head -1 | cut -d' ' -f 2 &)
-			echo "$i) $urls ---> $code"
+			echo -e "\033[1;33m$i) $urls ---> $code"
 		else
 			echo -n "$i) $urls ---> "
 			ping $urls
