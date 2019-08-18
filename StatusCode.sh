7c63a7e940f27acc7f8f645404e3b96f1daf443b
Nihal <37813784+iamnihal@users.noreply.github.com>
2019-08-18 11:13:58 +0530

    A huge performance improvement.
    Removed the use of "ping" command for not found or an invalid URL. This brings massive enhancement in performance of the script.

--- StatusCode.sh
+++ StatusCode.sh
@@ -8,34 +8,31 @@ logo()
 		\___ \| __/ _` | __| | | / __| | |   / _ \ / _` |/ _ \
 		 ___) | || (_| | |_| |_| \__ \ | |__| (_) | (_| |  __/
 		|____/ \__\__,_|\__|\__,_|___/  \____\___/ \__,_|\___|
-
-
 EOT
-}
+	}
 
-clear
-logo
-if [ -z "$1" ]
-then
-	echo -e "\033[1;31mNo argument provided!!"
-	sleep 2
-else
-	n=$(cat $1 | wc -l)
-	echo -e "\033[1;31m+-----------------------+"
-	echo -e "\033[1;31m|  $n URLs to be check  |"
-	echo -e "\033[1;31m+-----------------------+"
-	i=1
-	for urls in $(cat $1)
-	do
-		response=$(curl -Is "$urls")
-		if [ "$?" -eq "0" ]
-		then
-			code=$(echo "$response" | head -1 | cut -d' ' -f 2)
-			echo -e "\033[1;33m$i) $urls ---> $code"
-		else
-			echo -n "$i) $urls ---> "
-			ping $urls
-		fi
-		i=$(( $i + 1 ))
-	done
-fi
+	clear
+	logo
+	if [ -z "$1" ]
+	then
+		echo -e "\033[1;31mNo argument provided!!"
+		sleep 2
+	else
+		n=$(cat $1 | wc -l)
+		echo -e "\033[1;31m+-----------------------+"
+		echo -e "\033[1;31m|  $n URLs to be check  |"
+		echo -e "\033[1;31m+-----------------------+"
+		i=1
+		for urls in $(cat $1)
+		do
+			response=$(curl -ISs "$urls" 2>&1)
+			if [ "$?" -eq "0" ]
+			then
+				code=$(echo "$response" | head -1 | cut -d' ' -f 2)
+				echo -e "\033[1;33m$i) $urls ---> $code"
+			else
+				echo -ne "$i) $response\n"
+			fi
+			i=$(( $i + 1 ))
+		done
+	fi
